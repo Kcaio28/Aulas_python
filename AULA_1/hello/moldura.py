9ea9e6ac63ccd72baad20c4a1f2eb3c79b1ed4aa
CAIO CARNEIRO FERRER MORAES <25170980@educ123.sp.senai.br>
2025-04-29 15:48:06 -0300

    Add hello e moldura

---
+++ AULA_1/hello/moldura.py
@@ -0,0 +1,11 @@
+frase = input('\nDigite seu nome:')
+
+ch = '\u2588'
+
+for i in range(len(frase)+4):
+    print(ch, end='')
+
+print(f'\n{ch} {frase} {ch}')
+
+for i in range(len(frase)+4):
+    print(ch, end='')
